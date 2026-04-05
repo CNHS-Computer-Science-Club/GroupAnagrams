87f428ad06ca7bd57f918434cfd21bec9db229b7
CNHS Computer Science Club <csclub.cnhs@gmail.com>
2026-04-05 18:57:39 -0400

    Create StarterCode.java

---
+++ StarterCode.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] words) {
+        // your code here
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        // output is [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
+        System.out.println(groupAnagrams(words));
+    }
+}
